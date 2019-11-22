e3dbf0036fa82cbeaba57df3ad9844a2c7fbe4c0
Chris Saless <nonbusiness.chris@gmail.com>
2019-11-22 14:43:06 -0500

    base profile page has been setup

--- routes/profile.js
+++ routes/profile.js
@@ -18,4 +18,20 @@ router.get('/settings', (req, res) => {
     res.render('settings')
 });
 
+router.post('/deleteUser', (req, res) => {
+
+    // Delete everything to do with this user's workout data.
+
+
+    res.redirect(302, '/');
+});
+
+router.post('/deleteData', (req, res) => {
+
+    // Delete everything to do with this user.
+
+
+    res.redirect(302, '/');
+});
+
 module.exports = router;
