643cd816b7eb6dfd5e2a2a42cff2b0d5c36bd064
Chris Saless <nonbusiness.chris@gmail.com>
2019-11-26 10:03:40 -0500

    /news/add now requires auth

--- routes/news.js
+++ routes/news.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 
-router.all('/*', (req, res ,next) => {
+router.all('/*', (req, res, next) => {
     next();
 });
 
 router.get('/', (req, res) => {
     res.render('news');
+});
+
+router.get('/add', (req, res) => {
+
+    if (!req.isAuthenticated() || req.user == null || !req.user.isAdmin) {
+        res.render('denied');
+        return;
+    }
+
+    res.render('addUpdate');
+});
+
+router.post('/add', (req, res) => {
+    console.log(req.body.title);
+    console.log(req.body.subtitle);
+    console.log(req.body.description);
+    console.log(req.body.banner);
 });
 
 module.exports = router;
