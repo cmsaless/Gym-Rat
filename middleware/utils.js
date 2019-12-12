1ac2f21a7554d33aa87b22d8ace8c9841d24d804
Chris Saless <nonbusiness.chris@gmail.com>
2019-12-12 09:54:46 -0500

    separating the css files out even more

--- middleware/utils.js
+++ middleware/utils.js
@@ -2,7 +2,7 @@ module.exports = {
     formatDate: function (date) {
         let hours = date.getHours();
         let minutes = date.getMinutes();
-        let ampm = hours >= 12 ? 'pm' : 'am';
+        let ampm = 12 <= hours ? 'pm' : 'am';
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
         minutes = minutes < 10 ? '0' + minutes : minutes;
